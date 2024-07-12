ebd6bffa2de31f2ed562fc25013e70e22bda37cc
vansh3007 <vanshshrivastava30@gmail.com>
2024-07-12 21:41:22 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // main.jsx or index.jsx (entry file)
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 
 const container = document.getElementById("root");
